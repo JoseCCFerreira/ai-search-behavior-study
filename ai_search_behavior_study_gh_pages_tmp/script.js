23a4879087e1fc15382e3555a2ab3c1261a07e11
José Ferreira <carloscoelho123@gmail.com>
2026-06-02 10:55:34 +0100

    Publish portfolio site to gh-pages

---
+++ ai_search_behavior_study_gh_pages_tmp/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll("a[href^='#']");
+  links.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      if (target) {
+        target.scrollIntoView({ behavior: "smooth", block: "start" });
+      }
+    });
+  });
+});
